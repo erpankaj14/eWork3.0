b95e8582f0c1dae6050de06d75089919bc39d0fd
Rahul <rahulbhardwaz2k1@gmail.com>
2026-09-22 15:44:13 +0530

    cghanges

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { planMockInterceptor } from './core/interceptors/plan-mock.interceptor';
 import { provideToastr } from 'ngx-toastr';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -16,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideCharts(withDefaultRegisterables()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, planMockInterceptor])),
     provideTranslateService({
       fallbackLang: 'en'
     }),
